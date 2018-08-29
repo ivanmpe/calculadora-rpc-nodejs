f0deb49349062fd8a29768b9218049ecdcc5f2ec
Ivan Sousa <ivanmpe@users.noreply.github.com>
2018-08-29 18:16:25 -0300

    ajustes

--- serverRPC.js
+++ serverRPC.js
@@ -9,17 +9,17 @@ server.addHandler('soma', function(req, res, next) {
 server.addHandler('sub', function(req, res, next) {
     sub = req.m.a - req.m.b;
     console.log("subtracao");
-    res.write(sub);      // first response
+    res.write(sub);      
 })
 server.addHandler('mult', function(req, res, next) {
     console.log("multiplicacao");
 	var mult = req.m.a * req.m.b;
-    res.write(mult);      // first response
+    res.write(mult);      
 })
 server.addHandler('div', function(req, res, next) {
     console.log("divisao");
 	var div = req.m.a/req.m.b;
-    res.write(div);      // first response
+    res.write(div);     
 })
 
 server.listen(1337, function() {
